568c6928fbc0acefbda65fa940084508ff547878
Greg Tyler <git@gregtyler.co.uk>
2018-03-05 10:17:49 +0000

    Add sorting methods on notes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template lang="html">
   <ol v-if="$store.state.app.isLoaded">
-    <li v-for="note in $store.state.notes">{{note.title}}</li>
+    <li v-for="note in sortBy($store.state.notes, 'updatedAt', true)">{{note.title}}</li>
   </ol>
 </template>
 
 <script>
 export default {
+  methods: {
+    sortBy(arr, key, isDescending) {
+      return arr.slice().sort((a, b) => {
+        return (a[key] - b[key]) * (isDescending ? -1 : 1);
+      });
+    }
+  }
 }
 </script>
 
