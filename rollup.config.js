534c945f423e104476bd82ce696e6953441fe10b
Greg Tyler <git@gregtyler.co.uk>
2018-03-05 09:37:30 +0000

    Initial framework

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import json from 'rollup-plugin-json';
+import vue from 'rollup-plugin-vue';
+import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    file: 'public/bundle.js',
+    format: 'iife'
+  },
+  watch: {
+    include: ['src/**/*.vue', 'src/**/*.js', 'store/**/*.js'],
+    clearScreen: false
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    json({
+      preferConst: true
+    }),
+    vue({
+      css: true
+    }),
+    resolve()
+  ]
+};
