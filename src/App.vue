6bca869b77aa36062ad8c7fa25f3e89708a22ea9
Greg Tyler <git@gregtyler.co.uk>
2018-03-05 11:12:59 +0000

    Add basic material design layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template lang="html">
-  <ol v-if="$store.state.app.isLoaded">
-    <li v-for="note in sortBy($store.state.notes, 'updatedAt', true)">{{note.title}}</li>
-    <button type="button" @click="addNote">Add note</button>
-  </ol>
+  <div>
+    <AppBar />
+    <!--router-view /-->
+    <template v-if="$store.state.app.isLoaded">
+      <div v-for="note in sortBy($store.state.notes, 'updatedAt', true)" :key="note.id" class="card">
+        {{ note.title }}
+      </div>
+      <button class="action-button" type="button" @click="addNote">+</button>
+    </template>
+  </div>
 </template>
 
 <script>
+import AppBar from './components/AppBar.vue';
+
 export default {
+  components: {
+    AppBar
+  },
   methods: {
     sortBy(arr, key, isDescending) {
       return arr.slice().sort((a, b) => {
@@ -17,8 +28,37 @@ export default {
       this.$store.dispatch('ADD_NOTE', {type: 'text', title: 'Test!', body: 'Body'});
     }
   }
-}
+};
 </script>
 
 <style lang="css">
+.card {
+  margin: .5rem;
+  padding: .5rem;
+  background-color: #FFF;
+  border-radius: .1rem;
+  box-shadow: 0 2px .25rem hsla(0, 0%, 0%, 0.1);
+}
+
+.action-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  width: 3rem;
+  height: 3rem;
+  padding: 1rem;
+  border: none;
+  border-radius: 100%;
+  background-color: var(--color-secondary);
+  color: var(--color-secondary-text);
+  font-size: 1rem;
+  box-shadow: 0 .5rem .25rem hsla(0, 0%, 0%, 0.1);
+}
+
+.action-button:active {
+  background-color: #ff6090;
+}
 </style>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="app-bar">
+    Notes
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="css">
+.app-bar {
+  position: fixed;
+  padding: 1rem;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: var(--color-primary);
+  color: var(--color-primary-text);
+  font-size: 1.2rem;
+  box-shadow: 0 2px .25rem hsla(0, 0%, 0%, 0.2);
+}
+</style>
