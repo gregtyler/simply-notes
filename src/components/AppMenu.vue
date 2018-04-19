919129ef5e919707a68ecce49ca729c327a45693
Greg Tyler <git@gregtyler.co.uk>
2018-04-20 00:09:47 +0100

    Add menu header, tidy menu typography

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,7 +1,9 @@
 <template lang="html">
   <div class="app-menu__container">
     <div :class="{'app-menu__backdrop': true, 'app-menu__backdrop--visible': visible}" @click.self="$emit('close')" />
+
     <menu :class="{'app-menu': true, 'app-menu--visible': visible}">
+      <li class="app-menu__header"></li>
       <li>
         <router-link :to="{name: 'home'}" :class="{'app-menu__action': true, 'app-menu__action--active': $route.name === 'home'}" @click.native="$emit('close')">
           <UiIcon type="home" /> Home
@@ -71,10 +73,17 @@ export default {
   transform: translateX(0%);
 }
 
+.app-menu__header {
+  min-height: 140px;
+  background-color: var(--color-primary);
+  background-image: linear-gradient(16deg, transparent 33%, rgba(0, 0, 0, 0.3) 33%, rgba(0, 0, 0, 0.3) 66%, transparent 66%);
+}
+
 .app-menu__action {
   display: flex;
   padding: .5rem;
-  color: inherit;
+  font-size: 0.9rem;
+  font-weight: 500;
   text-decoration: none;
   align-items: center;
 }
